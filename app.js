70bc695c3e67edac464a747230964cd73b5591ed
Romina <romina.salminci@gmail.com>
2023-02-26 20:30:54 -0300

    Poyecto adaptado para Render

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const publicPath = path.resolve(__dirname,"./public");
 
 app.use(express.static(publicPath));
 
-app.listen(3000,()=> {
-    console.log ("Servidor corriendo en el puerto 3000");
-});
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>
+    console.log ("Servidor corriendo en el puerto http://localhost:" + port)
+);
 
 app.get ("/",(req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
